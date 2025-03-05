4a9d8d2058852e7ae536f9ae557f373ab1603a5c
Jephte Clain <Jephte.Clain@univ-reunion.fr>
2025-03-05 10:43:48 +0400

    corriger le calcul de la période courante

--- v26/00-periodes.sql
+++ v26/00-periodes.sql
@@ -3,8 +3,22 @@ drop table if exists schema_fcamanager.periode;
 create table schema_fcamanager.periode as
 select id
 , code
-, (first_value(code) over (order by annee_universitaire desc) = code) as courante
+, (now() between date_debut_validite and date_fin_validite) tem_dates
+, false courante
 from schema_odf.espace
 where temoin_active
 and type_espace = 'P'
+;
+
+-- sélectioner comme courante la première période dont la période de validité
+-- englobe la date du jour
+with codes as (
+select min(code) min
+from schema_fcamanager.periode
+where tem_dates
+)
+update schema_fcamanager.periode
+set courante = true
+from codes
+where code = codes.min
 ;
